99672689a24652a37f15927447c7bc123ef4afd1
Richard Morgan <morganric@gmail.com>
2013-08-05 15:38:03 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbtab::Application.config.secret_token = '59b1f4b6d89c60ec3ef82c4eb1ffc912aba9dcf5a239327f2ce5be88e2adcd0968980107415c87d44088478f7e2349c6ebee0c16368199d537e57b5d167675e5'
