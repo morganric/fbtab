3a85ef1b38a1211789300f6350ee3918229b7ad5
Richard Morgan <morganric@gmail.com>
2013-08-06 09:27:37 +0100

    comment out

--- config/environment.rb
+++ config/environment.rb
@@ -4,6 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Fbtab::Application.initialize!
 
-config.action_controller.allow_forgery_protection = false
+# config.action_controller.allow_forgery_protection = false
 config.gem "koala"
 
