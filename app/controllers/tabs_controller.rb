e8f6d5107532ee316e6a82e54673a79eec1e3698
Richard Morgan <morganric@gmail.com>
2013-08-06 10:51:43 +0100

    base64

--- app/controllers/tabs_controller.rb
+++ app/controllers/tabs_controller.rb
@@ -37,6 +37,21 @@ class TabsController < ApplicationController
       @data = @oauth.parse_signed_request(signed_request)
       @sr = @oauth.parse_signed_request(params["signed_request"])  
     end
+
+     def base64_url_decode str
+     encoded_str = str.gsub('-','+').gsub('_','/')
+     encoded_str += '=' while !(encoded_str.size % 4).zero?
+     Base64.decode64(encoded_str)
+    end
+
+    def decode_data str
+     encoded_sig, payload = str.split('.')
+     data = ActiveSupport::JSON.decode base64_url_decode(payload)
+    end
+
+    signed_request = params[:signed_request]
+    @signed_request = decode_data(signed_request)
+
    
 
     respond_to do |format|
